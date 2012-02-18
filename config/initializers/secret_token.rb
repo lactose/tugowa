cf41edcd9f3c1f2214b265f8bb8b84c0e84cef19
tugowa <tugowa@nibiru.(none)>
2012-02-18 14:52:16 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Www::Application.config.secret_token = 'ca57abaa3659402115851f9d96dd46d3fd0f76346b17b228dc6681528f390f8e8909789524ab8e08bb868056d5c317262de404b3ab43ac87836ed4e0a2dc60ae'
