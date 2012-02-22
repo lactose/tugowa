39946d20e98f089f4b1df46ba89426b815df7c73
Tugowa Server <root@tugowa.com>
2012-02-22 13:46:30 -0500

    added email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ActiveRecord::Base
+  validates :email, :presence => true, :format => { :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, message => "Email is invalid." }
 end
