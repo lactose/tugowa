4d0b0f86b76f8b941d0b62f4b72a88a46a0e967d
Tugowa Server <root@tugowa.com>
2012-02-23 15:02:20 -0500

    added validation for team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,3 @@
 class Team < ActiveRecord::Base
+  validates :name, :presence => true, :message => "Team must have a name."
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ActiveRecord::Base
-  validates :email, :presence => true, :format => { :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, :message => "Email is invalid." }
+  validates :email, :presence => true, :format => { :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, :message => "The e-mail address is invalid." }
 end
