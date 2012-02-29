1ff452adb05196382ffac0d6236e07fba99de73d
Zachary Owen <dev@scrptr.com>
2012-02-29 00:11:57 -0500

    added some user model tests..which throw errors for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ActiveRecord::Base
   has_many :sheets
   has_one :team
   validates_associated :sheets 
-  validates :email, :presence => true, :uniqueness => true, :format => { :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, :message => "The e-mail address is invalid." }
+  validates :email, :presence => true, 
+                    :format => { :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, :message => "The e-mail address is invalid." },
+                    :uniqueness => { :case_sensitive => false }
   # uncomment this line when the form has a tos
   # validates :terms_of_service, :acceptance => true
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  @attr = {
+    :name => "Example User",
+    :email => "user@example.com"
+  }
+
+  test "should not save user without email" do
+    User.new(@attr.merge(:email => ""))
+    assert false
+  end
+
+  test "should not save user with invalid email" do
+    User.new(@attr.merge(:email => "sdfaf"))
+    assert false
+  end
+
 end
