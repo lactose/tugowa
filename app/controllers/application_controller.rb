9e9d31871cc689f28a7e028305d98366c64f4dfd
Zachary Owen <dev@scrptr.com>
2012-02-28 22:40:17 -0500

    switched render authorize text to redirect to specific controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,8 @@ class ApplicationController < ActionController::Base
   def protect
     @ips = ['127.0.0.1', '69.164.213.39'] 
     if not @ips.include? request.remote_ip
-      # Check for your subnet stuff here, for example
-      # if not request.remote_ip.include?('127.0,0')
-      render :text => "You are unauthorized"
+      # if the user is coming from an outside ip, redirect
+      redirect_to :controller => 'home', :action => 'index'
       return
     end
   end
