cab1a066d264e2f08a7a092906202fe078ee7603
Zachary Owen <dev@scrptr.com>
2012-03-07 16:47:39 -0500

    added factory_girl and started messing with the user controller rspec tests..pt2: missed a file

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Zachary Owen"
+  user.email                 "zac@example.com"
+  user.password              "moobar"
+  user.password_confirmation "moobar"
+end
