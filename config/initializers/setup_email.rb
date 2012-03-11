6bf4b2146d9c190eafc83bc1f409a95f4b1b3e04
Zachary Owen <dev@scrptr.com>
2012-03-10 19:25:59 -0500

    started adding user email confirmation..working link and emails....pt3

---
+++ config/initializers/setup_email.rb
@@ -0,0 +1,11 @@
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => ENV['GMAIL_SMTP_USER'],
+  :password             => ENV['GMAIL_SMTP_PASSWORD'],
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
+
+ActionMailer::Base.default_url_options[:host] = Rails.env.production? ? "tugowa.com" : "localhost:3000"
+#Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
---
+++ db/migrate/20120310221839_add_confirm_code_to_users.rb
@@ -0,0 +1,6 @@
+class AddConfirmCodeToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :confirm_code, :string
+
+  end
+end
