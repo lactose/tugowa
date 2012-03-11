2e84b3395c712c3c98a58288ffce68ba90f73bb2
Zachary Owen <dev@scrptr.com>
2012-03-10 23:31:12 -0500

    production mails failing and need to see errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Www::Application.configure do
   config.assets.precompile += %w( bootstrap.css )
 
   # Disable delivery errors, bad email addresses will be ignored
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
 
   # Enable threaded mode
