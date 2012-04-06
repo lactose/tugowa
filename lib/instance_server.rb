02efeb7fd5d85a6c225bded84b597ca37421be37
Zachary Owen <dev@scrptr.com>
2012-04-06 08:20:58 -0400

    instance server created

--- lib/instance_server.rb
+++ lib/instance_server.rb
@@ -3,3 +3,22 @@ require 'eventmachine'
 require 'em-websocket'
 require 'pg'
 
+module InstanceServer
+  def post_init
+    puts "-- someone connected --"
+  end
+
+  def receive_data data
+    send_data ">>>you sent: #{data}"
+    close_connection if data =~ /quit/i
+  end
+
+  def unbind
+    puts "-- someone disconnected --"
+  end
+
+end
+
+EventMachine::run {
+  EventMachine::start_server "127.0.0.1", 8081, InstanceServer
+}
